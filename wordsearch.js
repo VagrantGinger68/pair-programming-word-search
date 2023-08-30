9c566d7093db42e54d3a1fafb9d98980c9b5c9bd
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-30 18:04:43 -0400

    Made the wordsearch function and added new test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const verticalJoin = [];
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  for (let col = 0; col < letters.length; col++) {
+    let tempCol = "";
+    for (let row = 0; row < letters[col].length; row++) {
+      tempCol += letters[row][col];
+    }
+    verticalJoin.push(tempCol);
+  }
+  for (let letter of verticalJoin) {
+    if (letter.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
